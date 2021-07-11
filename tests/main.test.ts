8674c74b971dc28d728a13f4a4256392783db0a4
Junior <sudojunior@outlook.com>
2021-07-11 20:10:21 +0100

    Exchange jest and eslint for xo and ava

---
+++ tests/main.test.ts
@@ -0,0 +1,52 @@
+import t from 'ava'
+import { context } from '@actions/github'
+
+import { accessGroups } from '../src/access'
+
+const ENV = process.env
+
+// Should figure out a way to do matrix-like testing offline...
+// ... and to write tests correctly
+t.before(() => {
+	Object.assign(process.env, {
+		GITHUB_REPOSITORY: 'sudojunior/access-groups',
+		GITHUB_ACTOR: 'sudojunior'
+	})
+})
+
+t.after(() => {
+	process.env = { ...ENV }
+})
+
+const { groups, highestGroup } = accessGroups(context, {
+	viewer: { isSiteAdmin: false }
+})
+
+t('actor is not a site admin', expect => {
+	expect.assert(!groups.includes('site admin'))
+})
+
+t('actor is repository owner', expect => {
+	expect.assert(groups.includes('repo owner'))
+})
+
+t("actor's highest group is 'repo owner'", expect => {
+	expect.is(highestGroup, 'repo owner')
+})
+
+t.todo('actor is a repository collaborator')
+// #region organization permissions
+t.todo('actor is an organization member (skip if non-org owner)')
+t.todo('actor is a repository admin')
+t.todo('actor is a repository maintainer')
+t.todo('actor is a repository triage member')
+t.todo('actor is author of issue / pull request in the current scope')
+// #endregion
+t.todo('actor is a campus expert')
+t.todo('actor is a bounty hunter')
+t.todo('actor is a developer program member')
+t.todo(
+	'actor is a sponsor to the owner of the repository (or to deligated member?)'
+)
+t.todo('actor is an observer (private repository read access)')
+t.todo('actor is a stargazer to the repository')
