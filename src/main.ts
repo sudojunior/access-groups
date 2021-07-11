392aab5ba2020d9a1c0fbc5723b799a3718f5160
Junior <sudojunior@outlook.com>
2021-07-11 23:30:04 +0100

    add arguments to query + reformat main file

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,45 @@
 import * as core from '@actions/core';
-import { context, getOctokit } from '@actions/github';
-import { OctokitResponse } from '@octokit/types';
+import {context, getOctokit} from '@actions/github';
+import {OctokitResponse} from '@octokit/types';
 
-import { accessGroups } from './access';
-import { DataQuery, logAndExport } from './util';
+import {accessGroups} from './access';
+import {DataQuery, logAndExport} from './util';
 
 async function run(): Promise<void> {
 	try {
-		const { actor } = context; // core.getInput('user') - future
-		const { owner, repo } = context.repo;
+		const {actor} = context; // core.getInput('user') - future
+		const {owner, repo} = context.repo;
 
-		const token: string = core.getInput('github-token', { required: false });
+		const token: string = core.getInput('github-token', {required: false});
 
 		const octokit = getOctokit(token, {
 			baseUrl: context.apiUrl,
 		});
 
-		const { data }: OctokitResponse<DataQuery> = await octokit.graphql(
+		const {data}: OctokitResponse<DataQuery> = await octokit.graphql(
 			`
-      query data {
-        user(login: $actor) {
-          ...Access
-        }
-        repository(owner: $owner, name: $repo) {
-          collaborators(query: $actor) {
-            ...Access
-          }
-        }
-      }
-      
-      fragment Access on User {
-        login
-        isViewer
-        isSiteAdmin
-      }
+      query accessData($owner: String!, $actor: String!, $repo: String!) {
+				user(login: $actor) {
+					...Access
+				}
+				repository(owner: $owner, name: $repo) {
+					owner {
+						...Access
+					}
+					collaborators(query: $actor) {
+						nodes {
+							...Access
+							
+						}
+					}
+				}
+			}
+			
+			fragment Access on User {
+				login
+				isViewer
+				isSiteAdmin
+			}
       `,
 			{
 				owner,
@@ -44,17 +50,12 @@ async function run(): Promise<void> {
 
 		core.debug(`Access Data: ${JSON.stringify(data)}`);
 
-		const { groups, highestGroup } = accessGroups(context, data);
+		const {groups, highestGroup} = accessGroups(context, data);
 
 		logAndExport('groups', groups, `${context.actor} has access to %s`);
 		logAndExport('highest-group', highestGroup, `${context.actor} groups.first -> %s`);
-	} catch (err: unknown) {
-		const error = err as Error
-		
-		if (error.stack) {
-			core.debug(error.stack);
-		}
-		core.setFailed(error.message);
+	} catch (error: unknown) {
+		core.setFailed((error as Error).message);
 	}
 }
 
