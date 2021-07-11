30f93c5b169224bdb81aa212e5e0d12d51bb367c
Junior <sudojunior@outlook.com>
2021-07-11 23:38:39 +0100

    add debug step

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import * as core from '@actions/core';
-import {context, getOctokit} from '@actions/github';
-import {OctokitResponse} from '@octokit/types';
+import { context, getOctokit } from '@actions/github';
+import { OctokitResponse } from '@octokit/types';
 
-import {accessGroups} from './access';
-import {DataQuery, logAndExport} from './util';
+import { accessGroups } from './access';
+import { DataQuery, logAndExport } from './util';
 
 async function run(): Promise<void> {
 	try {
-		const {actor} = context; // core.getInput('user') - future
-		const {owner, repo} = context.repo;
+		const { actor } = context; // core.getInput('user') - future
+		const { owner, repo } = context.repo;
 
-		const token: string = core.getInput('github-token', {required: false});
+		const token: string = core.getInput('github-token', { required: false });
 
 		const octokit = getOctokit(token, {
 			baseUrl: context.apiUrl,
 		});
 
-		const {data}: OctokitResponse<DataQuery> = await octokit.graphql(
+		const { data }: OctokitResponse<DataQuery> = await octokit.graphql(
 			`
       query accessData($owner: String!, $actor: String!, $repo: String!) {
 				user(login: $actor) {
@@ -50,7 +50,7 @@ async function run(): Promise<void> {
 
 		core.debug(`Access Data: ${JSON.stringify(data)}`);
 
-		const {groups, highestGroup} = accessGroups(context, data);
+		const { groups, highestGroup } = accessGroups(context, data);
 
 		logAndExport('groups', groups, `${context.actor} has access to %s`);
 		logAndExport('highest-group', highestGroup, `${context.actor} groups.first -> %s`);
