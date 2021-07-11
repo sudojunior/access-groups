70649028d155c4e74fe6b57124bc1fbc7d7df1ec
Junior <sudojunior@outlook.com>
2021-07-11 20:36:50 +0100

    Add condition builder for group checks

---
+++ src/condition.ts
@@ -0,0 +1,27 @@
+import {Context} from '@actions/github/lib/context';
+import {DataQuery} from './util';
+
+export interface Condition {
+	key: string;
+	check: ConditionPredicate;
+}
+
+type ConditionPredicate = (data: DataQuery, context: Context) => boolean;
+
+export default class ConditionBuilder {
+	constructor(private readonly key: string) {}
+
+	useCheck(predicate: ConditionPredicate): ConditionBuilder {
+		this.check = (data, context) => predicate(data, context);
+		return this;
+	}
+
+	build(): Condition {
+		return {
+			key: this.key,
+			check: this.check,
+		};
+	}
+
+	private check: ConditionPredicate = () => false;
+}
