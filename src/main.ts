ce5564d455d72b190b69f556708e406a34f8e62b
Junior <sudojunior@outlook.com>
2021-07-11 22:03:25 +0100

    revert: make token a required argument (d8fe5f4)

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import * as core from '@actions/core';
-import {context, getOctokit} from '@actions/github';
-import {OctokitResponse} from '@octokit/types';
+import { context, getOctokit } from '@actions/github';
+import { OctokitResponse } from '@octokit/types';
 
-import {accessGroups} from './access';
-import {DataQuery, logAndExport} from './util';
+import { accessGroups } from './access';
+import { DataQuery, logAndExport } from './util';
 
 async function run(): Promise<void> {
 	try {
-		const {actor} = context; // core.getInput('user') - future
-		const {owner, repo} = context.repo;
+		const { actor } = context; // core.getInput('user') - future
+		const { owner, repo } = context.repo;
 
-		const token: string = core.getInput('token', {required: true});
+		const token: string = core.getInput('token', { required: false });
 
 		const octokit = getOctokit(token, {
 			baseUrl: context.apiUrl,
 		});
 
-		const {data}: OctokitResponse<DataQuery> = await octokit.graphql(
+		const { data }: OctokitResponse<DataQuery> = await octokit.graphql(
 			`
       query data {
         user(login: $actor) {
@@ -44,12 +44,17 @@ async function run(): Promise<void> {
 
 		core.debug(`Access Data: ${JSON.stringify(data)}`);
 
-		const {groups, highestGroup} = accessGroups(context, data);
+		const { groups, highestGroup } = accessGroups(context, data);
 
 		logAndExport('groups', groups, `${context.actor} has access to %s`);
 		logAndExport('highest-group', highestGroup, `${context.actor} groups.first -> %s`);
-	} catch (error: unknown) {
-		core.setFailed((error as Error).message);
+	} catch (err: unknown) {
+		const error = err as Error
+		
+		if (error.stack) {
+			core.debug(error.stack);
+		}
+		core.setFailed(error.message);
 	}
 }
 
